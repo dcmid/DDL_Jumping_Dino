2d066dd915f8a3abc9969c91ed7564d735678d01
Midkiff <Darren.C.Midkiff-1@ou.edu>
2019-04-25 16:42:31 -0500

    got spacebar input working and turning on LED

--- dino_jump.c
+++ dino_jump.c
@@ -13,6 +13,7 @@
 #endif
 
 #include <cr_section_macros.h>
+#include <stdio.h>
 
 // TODO: insert other include files here
 
@@ -20,13 +21,14 @@
 #define PINMODE0 (*(volatile unsigned int*)0x4002C040) //pin resistor mode reg
 #define FIO0PIN0 (*(volatile unsigned int*)0x2009C014) //pin value reg
 
-//#define EXTINT (*(volatile unsigned int*)0x400FC140) //external interrupt flag reg
-//#define EXTMODE (*(voaltile unsigned int*)0x400FC148) //external interrupt mode reg
-//#define EXTPOLAR (*(volatile unsigned int*)0x400FC14C) //external interrupt polarity reg
+#define IO0IntEnF (*(volatile unsigned int*)0x40028094)
+#define IO0IntClr (*(volatile unsigned int*)0x4002808C)
+#define ISER0 (*(volatile unsigned int*)0xE000E100)
 
+#define SCK ((FIO0PIN0 >> 0) & 1) //SCK (ps2 clock) is at P0.0 (pin 9)
+#define SDA ((FIO0PIN0 >> 1) & 1) //SDA (ps2 data) is at P0.1 (pin 10)
 
-#define SCK ((FIO0PIN0 >> 0) & 1)
-#define SDA ((FIO0PIN0 >> 1) & 1)
+#define SPACEBAR 0b01001010001
 
 //void busout_8bit(unsigned int output){
 //	for(int i=0; i<32; i++){
@@ -37,15 +39,35 @@
 //	}
 //}
 
+int num_clocks = 0;
+int space_pressed = 0;
+unsigned int ps2_data = 0;
+
+void shift_in(int bit_in){
+	ps2_data = ps2_data<<1; //shift ps2_data
+	ps2_data |= (bit_in<<0); //insert bit_in
+	ps2_data &= 0b11111111111; //preserve only bottom 11 bits
+}
+
+void EINT3_IRQHandler(void){//executes on falling edge of SCK (ps2 clock)
+	shift_in(SDA);//shift SDA into ps2_data
+	num_clocks++;//increment ps2 clock count
+	if(num_clocks == 11){//when 11 bits have been read, compare to spacebar encoding
+		space_pressed = (ps2_data == SPACEBAR);
+		num_clocks = 0;//reset clock ps2 count
+		printf("%d\n", ps2_data);
+	}
+	IO0IntClr |= (1<<0);//clear interrupt
+}
+
 int main(void) {
 
-    // TODO: insert code here
+	IO0IntEnF = (1<<0);//enable interrupts on falling edge of P0.0 (Pin 9)
+	ISER0 |= (1<<21);//enable interrupts from EINT3 (GPIO)
+	FIO0DIR = (1<<6);//set 0.18 (pin 11) as output
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
     while(1) {
-        i++ ;
+    	FIO0PIN0 = (space_pressed<<6);
     }
     return 0 ;
 }
